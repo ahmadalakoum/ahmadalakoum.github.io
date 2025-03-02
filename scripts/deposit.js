45c8070a5710672e54ce5d0d3187abb153ebbd7b
Ahmad Al Akoum <aakoum02@gmail.com>
2025-03-02 21:51:42 +0200

    commit

---
+++ scripts/deposit.js
@@ -0,0 +1,30 @@
+document.getElementById("depositForm").addEventListener("submit",async (e)=>{
+    e.preventDefault();
+    const walletID = document.getElementById("wallet_id").value.trim();
+    const amount = document.getElementById("amount").value.trim();
+    const message = document.getElementById("responseMessage");
+    message.textContent = '';
+    if(!walletID ||!amount){
+        message.textContent = "All fields are required";
+        return;
+    }
+    const depositURL=`${config.apiBaseUrl}/wallet/deposit.php`;
+    const response = await fetch(depositURL,{
+        method:"POST",
+        headers:{
+            "Content-Type":"application/json",
+            "Authorization": `Bearer ${localStorage.getItem("userID")}}`
+        },
+        body:JSON.stringify({wallet_id:walletID, amount})
+    });
+    const result = await response.json();
+    if(response.ok){
+        message.style.color = "green";
+        message.textContent = result.message;
+        window.location.href="./view_wallets.html";
+    }else{
+        message.style.color = "red";
+        message.textContent = result.message;
+        document.getElementById("depositForm").reset();
+    }
+})
