906417cc0053a029a5a9f4a4c3b49613d20b50f1
Ahmad Al Akoum <aakoum02@gmail.com>
2025-03-02 21:31:09 +0200

    commit

--- scripts/view_wallets.js
+++ scripts/view_wallets.js
@@ -18,13 +18,10 @@ document.addEventListener("DOMContentLoaded",async (e)=>{
                 walletDiv.innerHTML = `
                     <h3>${wallet.wallet_name}</h3>
                     <p>Balance: ${wallet.balance} ${wallet.currency}</p>
-                    <a href="deposit.html?wallet=${encodeURIComponent(wallet.wallet_name)}">Deposit</a>
-                        <a href="withdraw.html?wallet=${encodeURIComponent(wallet.wallet_name)}">Withdraw</a>
-                        <a href="transfer.html?wallet=${encodeURIComponent(wallet.wallet_name)}">Transfer</a>
-                        <a href="history.html?wallet=${encodeURIComponent(wallet.wallet_name)}&type=transactions">Transactions</a>
-                        <a href="history.html?wallet=${encodeURIComponent(wallet.wallet_name)}&type=withdrawals">Withdrawals</a>
-                        <a href="history.html?wallet=${encodeURIComponent(wallet.wallet_name)}&type=deposits">Deposits</a>
-                        <a href="history.html?wallet=${encodeURIComponent(wallet.wallet_name)}&type=internal_transfers">Internal Transfers</a>
+                    <a href="deposit.html">Deposit</a>
+                        <a href="withdraw.html">Withdraw</a>
+                        <a href="transfer.html">Transfer</a>
+                        <a href="history.html">Transactions</a>
                 `;
 
                 walletsList.appendChild(walletDiv);
