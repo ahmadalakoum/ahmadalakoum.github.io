bcc4255f0d92c704a4fc4960a8d8bb61035bf3e3
Ahmad Al Akoum <aakoum02@gmail.com>
2025-03-03 12:04:52 +0200

    commit

--- scripts/transfer.js
+++ scripts/transfer.js
@@ -5,12 +5,12 @@ document.getElementById("transferForm").addEventListener("submit",async (e)=>{
     // Get the wallet_id from the query string
     const walletID = urlParams.get('wallet_id');
     const receiverUsername= document.getElementById("receiverUsername").value.trim();
-    const walletName= document.getElementById("wallet_name").value.trim();
+    const receiverWalletName= document.getElementById("wallet_name").value.trim();
     const amount = document.getElementById("amount").value.trim();
 
     const msg= document.getElementById("responseMessage");
     msg.textContent='';
-    if(!receiverUsername ||!walletName ||!amount){
+    if(!receiverUsername ||!receiverWalletName ||!amount){
         msg.textContent="All fields are required";
         return;
     }
@@ -22,7 +22,7 @@ document.getElementById("transferForm").addEventListener("submit",async (e)=>{
             "Content-Type":"application/json",
             "Authorization": `Bearer ${localStorage.getItem("userID")}`
         },
-        body:JSON.stringify({receiverUsername, walletName, amount})
+        body:JSON.stringify({receiverUsername, receiverWalletName, amount})
     })
     const result = await response.json();
     if(response.ok){
