346a34d93c1e7ba14d65d42b6c5be3238d8bad4e
Ahmad Al Akoum <aakoum02@gmail.com>
2025-03-03 11:52:59 +0200

    commit

---
+++ scripts/transfer.js
@@ -0,0 +1,35 @@
+document.getElementById("transferForm").addEventListener("submit",async (e)=>{
+    e.preventDefault();
+    const urlParams = new URLSearchParams(window.location.search);
+
+    // Get the wallet_id from the query string
+    const walletID = urlParams.get('wallet_id');
+    const receiverUsername= document.getElementById("receiverUsername").value.trim();
+    const walletName= document.getElementById("wallet_name").value.trim();
+    const amount = document.getElementById("amount").value.trim();
+
+    const msg= document.getElementById("responseMessage");
+    msg.textContent='';
+    if(!receiverUsername ||!walletName ||!amount){
+        msg.textContent="All fields are required";
+        return;
+    }
+    const userID=localStorage.getItem("userID");
+    const transferURL=`${config.apiBaseUrl}/wallet/transfer.php?senderWalletID=${walletID}`;
+    const response = await fetch(transferURL,{
+        method:"POST",
+        headers:{
+            "Content-Type":"application/json",
+            "Authorization": `Bearer ${localStorage.getItem("userID")}`
+        },
+        body:JSON.stringify({receiverUsername, walletName, amount})
+    })
+    const result = await response.json();
+    if(response.ok){
+        msg.style.color="green";
+        msg.textContent=result.message;
+    }else{
+        msg.style.color="red";
+        msg.textContent=result.message;
+    }
+})
